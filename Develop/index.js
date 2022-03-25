f858f60c12810ea9433c8307c9c893dacc8f10a3
TimK76 <haadelphos2001@gmail.com>
2022-03-25 14:30:49 -0400

    questions array added

--- Develop/index.js
+++ Develop/index.js
@@ -4,52 +4,148 @@ const inquirer = require('inquirer');
 const generateReadMe = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [{
-    type: "input",
-    name: "Title",
-    message: "What is the title of your project?"
-},
-{   type: "input",
-    name: "Description",
-    message: "Please write your project description here?"
-},
-{   type: "input",
-    name: "Table of Contents",
-    message: "What sections should be added to your table of contents?"
-},
-{   type: "input",
-    name: "Installation",
-    message: "How will your users install the project on their machines?"
-},
-{   type: "input",
-    name: "Usage",
-    message: "What is the usage of this project?"
-},
-{   type: "checkbox",
-    name: "License",
-    message: "which license would you like to apply to this project?",
-    choices: "Academic Free License v3.0, Educational Community License v2.0, MIT, Mozilla Public License 2.0"
-},
-{   type: "input",
-    name: "Contributers",
-    message: "whom would you like to credit for their contributions?",
-},
-{   type: "input",
-    name: "Tests",
-    message: "what tests have been run",
-},
-{   type: "list",
-    name: "Questions",
-    message: "Please include your contact information including the minimum of your name and email along with directions for how future users may contact you with their questions on the project.",
-},
-];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "Title",
+            message: "What is the title of your project?",
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the title of your project.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "Description",
+            message: "Please write your project description here?",
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of your project.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "Installation",
+            message: "How will your users install the project on their machines?",
+            validate: installationInput => {
+                if (installationInput) {
+                    return true;
+                } else {
+                    console.log('Please include installation directions for your users.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "Usage",
+            message: "What is the usage of this project?",
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log("Usage information is required.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "checkbox",
+            name: "License",
+            message: "Which license would you like to apply to this project?",
+            choices: ["Academic Free License v3.0", "Educational Community License v2.0", "MIT", "Mozilla Public License 2.0"],
+            validate: licenseChoices => {
+                if (licenseChoices) {
+                    return true;
+                } else {
+                    console.log('One Licence must be selected.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "Contributing",
+            message: "How may others contribute to this project?",
+            validate: contributingInput => {
+                if (contributingInput) {
+                    return true;
+                } else {
+                    console.log("Contribution guidelines are required.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "Tests",
+            message: ("Please enter testing instructions."),
+            validate: testsInput => {
+                if(testsInput) {
+                    return true;
+                } else {
+                    console.log("Testing instructions are required.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "contactName",
+            message: "Please include your name for users who need to contact you with their questions.",
+            validate: contactNameInput => {
+                if (contactNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name.');
+                    return false
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "contactEmail",
+            message: "Please include your email for users who need to contact you with their questions.",
+            validate: contactEmailInput => {
+                if (contactEmailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email.');
+                    return false
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "contactDirections",
+            message: "Please include directions for users who need to contact you with their questions.",
+            validate: contactDirectionsInput => {
+                if (contactDirectionsInput) {
+                    return true;
+                } else {
+                    console.log('Please enter directions for users needing to contact you with their questions about this project');
+                    return false
+                }
+            }
+        },
+])};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    // TODO: Create a function to write README file
+    function writeToFile(fileName, data) { }
 
-// TODO: Create a function to initialize app
-function init() {}
+    // TODO: Create a function to initialize app
+    function init() { }
 
-// Function call to initialize app
-init();
-console.log(hello, Tim)
+    // Function call to initialize app
+    init();
+
+    questions();
