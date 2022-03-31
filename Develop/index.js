041e0e5998c35f162503f996800b788dea5987d5
TimK76 <haadelphos2001@gmail.com>
2022-03-31 15:27:07 -0400

    working on init function

--- Develop/index.js
+++ Develop/index.js
@@ -2,150 +2,75 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateReadMe = require('./utils/generateMarkdown');
+const path = require('path');
 
 // TODO: Create an array of questions for user input
-const questions = () => {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "Title",
-            message: "What is the title of your project?",
-            validate: titleInput => {
-                if (titleInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the title of your project.');
-                    return false;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "Description",
-            message: "Please write your project description here?",
-            validate: descriptionInput => {
-                if (descriptionInput) {
-                    return true;
-                } else {
-                    console.log('Please enter a description of your project.');
-                    return false;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "Installation",
-            message: "How will your users install the project on their machines?",
-            validate: installationInput => {
-                if (installationInput) {
-                    return true;
-                } else {
-                    console.log('Please include installation directions for your users.');
-                    return false;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "Usage",
-            message: "What is the usage of this project?",
-            validate: usageInput => {
-                if (usageInput) {
-                    return true;
-                } else {
-                    console.log("Usage information is required.");
-                    return false;
-                }
-            }
-        },
-        {
-            type: "checkbox",
-            name: "License",
-            message: "Which license would you like to apply to this project?",
-            choices: ["Academic Free License v3.0", "Educational Community License v2.0", "MIT", "Mozilla Public License 2.0"],
-            validate: licenseChoices => {
-                if (licenseChoices) {
-                    return true;
-                } else {
-                    console.log('One Licence must be selected.');
-                    return false;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "Contributing",
-            message: "How may others contribute to this project?",
-            validate: contributingInput => {
-                if (contributingInput) {
-                    return true;
-                } else {
-                    console.log("Contribution guidelines are required.");
-                    return false;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "Tests",
-            message: ("Please enter testing instructions."),
-            validate: testsInput => {
-                if(testsInput) {
-                    return true;
-                } else {
-                    console.log("Testing instructions are required.");
-                    return false;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "contactName",
-            message: "Please include your name for users who need to contact you with their questions.",
-            validate: contactNameInput => {
-                if (contactNameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your name.');
-                    return false
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "contactEmail",
-            message: "Please include your email for users who need to contact you with their questions.",
-            validate: contactEmailInput => {
-                if (contactEmailInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your email.');
-                    return false
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "contactDirections",
-            message: "Please include directions for users who need to contact you with their questions.",
-            validate: contactDirectionsInput => {
-                if (contactDirectionsInput) {
-                    return true;
-                } else {
-                    console.log('Please enter directions for users needing to contact you with their questions about this project');
-                    return false
-                }
-            }
-        },
-])};
+const questions = [
+    {
+        type: "input",
+        name: "Title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "Please write your project description here?",
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "How will your users install the project on their machines?",
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "What is the usage of this project?",
+    },
+    {
+        type: "checkbox",
+        name: "License",
+        message: "Which license would you like to apply to this project?",
+        choices: ["Academic Free License v3.0", "Educational Community License v2.0", "MIT", "Mozilla Public License 2.0"],
+    },
+    {
+        type: "input",
+        name: "Contributing",
+        message: "How may others contribute to this project?",
+    },
+    {
+        type: "input",
+        name: "Tests",
+        message: "Please enter testing instructions.",
+    },
+    {
+        type: "input",
+        name: "contactName",
+        message: "Please include your name for users who need to contact you with their questions.",
+    },
+    {
+        type: "input",
+        name: "contactEmail",
+        message: "Please include your email for users who need to contact you with their questions.",
+    },
+    {
+        type: "input",
+        name: "contactDirections",
+        message: "Please include directions for users who need to contact you with their questions.",
+    },
+];
 
-    // TODO: Create a function to write README file
-    function writeToFile(fileName, data) { }
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
 
-    // TODO: Create a function to initialize app
-    function init() { }
-
-    // Function call to initialize app
-    init();
-
-    questions();
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then((answer) => {
+            console.log(answer)
+            writeToFile('readme.md', generateMarkdown(answer))
+        })
+}
+// Function call to initialize app
+init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,35 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+  # ${data.title}
+  ## [Description](${answer.description}
+  # Table of Contents
+      ### 1. [Description](${answer.description}).
+      ### 2. [Installation](${answer.installation}).
+      ### 3. [Usage](${answer.usage}).
+      ### 4. [License](${answer.license}).
+      ### 5. [Contibuting](${answer.contributing}).
+      ### 6. [Tests](${answer.tests}).
+      ### 7. [Questions](My name is ${answer.contactName}. If you have questions please follow these directions to contact me: ${answer.contactDirections}. If you've read these directions and agree to follow them then please email me at ${answer.contactEmail}. Thank you; I hope you'll enjoy using this application.).
+  ## Installation
+  ## Usage
+  ## License
+  ## Contributing
+  ## Tests
+  ## Questions
 `;
 }
 
